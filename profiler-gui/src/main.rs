9de83574cff629f5f8eb7f1f74caea55eb1b8fde
Peanutt42 <p3anutt42@gmail.com>
2024-01-23 14:18:11 +0100

    profiler-gui: Added tooltip that reveals the entire name

--- profiler-gui/src/main.rs
+++ profiler-gui/src/main.rs
@@ -17,6 +17,7 @@ fn main() -> eframe::Result<()>{
 	let mut zoom: f32 = 1.0;
 
 	let mut screen_width = 800.0;
+	let mut mouse_pos = egui::Pos2::new(0.0, 0.0);
 
 	let mut profiler: Option<ProcessedProfiler> = None;
 
@@ -39,12 +40,15 @@ fn main() -> eframe::Result<()>{
 
 		// timeline input
 		ctx.input(|i| {
+			if let Some(pos) = i.pointer.latest_pos() {
+				mouse_pos = pos;
+			}
+
 			for e in i.events.iter() {
 				match e {
 					egui::Event::MouseWheel { unit: _, delta, modifiers: _ } => {
 						let factor = delta.y * 0.15 + 1.0;
 						zoom *= factor;
-						let mouse_pos = i.pointer.latest_pos().unwrap_or_default();
 						offset -= (mouse_pos.x - (screen_width / 2.0)) / zoom * ((1.0 / factor) - 1.0);
 					}
 					_ => {},
@@ -80,19 +84,31 @@ fn main() -> eframe::Result<()>{
 
 				screen_width = ctx.screen_rect().width();
 				let center_x = screen_width / 2.0;
+				let height = 20.0;
 
 				for frame in profiler.frames.iter() {
 					for profile_result in frame.profile_results.iter() {
-						let x = (profile_result.start.as_secs_f32() * screen_width / total_time - offset) * zoom;
-						let x = center_x + (x);
+						let x = center_x + (profile_result.start.as_secs_f32() * screen_width / total_time - offset) * zoom;
+						let y = profile_result.depth as f32 * height;
 						let width = (profile_result.duration.as_secs_f32() / total_time) * screen_width * zoom;
 						
-						let rect_height = 20.0;
-						let rect = egui::Rect::from_min_size(egui::Pos2::new(x, profile_result.depth as f32 * rect_height), egui::Vec2::new(width, rect_height));
+						let rect = egui::Rect::from_min_size(egui::Pos2::new(x, y), egui::Vec2::new(width, height));
 						canvas.rect(rect, 2.5, egui::Color32::BLUE, egui::Stroke::new(1.5, egui::Color32::BLACK));
 
+						let mut allow_tooltip = false;
 						if width > 50.0 {
-							draw_truncated_text(&canvas, ui, &profile_result.name, width, rect.center());
+							let truncated = draw_truncated_text(&canvas, ui, &profile_result.name, width, rect.center());
+							if truncated {
+                                allow_tooltip = true;
+                            }
+						}
+						else {
+							allow_tooltip = true;
+						}
+						if allow_tooltip && mouse_pos.x >= x && mouse_pos.y >= y && mouse_pos.y <= y + height && mouse_pos.x <= x + width {
+							egui::show_tooltip_at_pointer(ctx, egui::Id::new("profiler_result_tooltip"), |ui| {
+								ui.label(&profile_result.name);
+							});
 						}
 					}
 				}
@@ -141,7 +157,8 @@ fn glyph_char_width(c: char, font_id: egui::FontId, ui: &mut egui::Ui) -> f32 {
 	ui.fonts(|f| f.glyph_width(&font_id, c))
 }
 
-fn draw_truncated_text(painter: &egui::Painter, ui: &mut egui::Ui, text: &str, max_width: f32, pos: egui::Pos2) {
+// returns wheter the text was truncated
+fn draw_truncated_text(painter: &egui::Painter, ui: &mut egui::Ui, text: &str, max_width: f32, pos: egui::Pos2) -> bool {
 	let font_id = egui::TextStyle::Body.resolve(ui.style());
 
     let text_width = glyph_width(text.to_string(), font_id.clone(), painter);
@@ -167,4 +184,6 @@ fn draw_truncated_text(painter: &egui::Painter, ui: &mut egui::Ui, text: &str, m
     };
 
     painter.text(pos, egui::Align2::CENTER_CENTER, truncated_text, font_id, egui::Color32::WHITE);
+
+	text_width > max_width
 }
