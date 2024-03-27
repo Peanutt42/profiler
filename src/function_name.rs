f6790da26d3ccc637b08c9193b7bf5be10f6f595
Peanutt42 <p3anutt42@gmail.com>
2024-03-27 13:01:56 +0100

    function_name makro now removes ::{{closure}}

--- src/function_name.rs
+++ src/function_name.rs
@@ -6,7 +6,17 @@ macro_rules! function_name {
         fn type_name_of<T>(_: T) -> &'static str {
             std::any::type_name::<T>()
         }
-        let name = type_name_of(f);
-        name.strip_suffix("::f").unwrap()
+        let name = type_name_of(f).strip_suffix("::f").unwrap();
+		let mut cleaned_name = String::new();
+		let mut prev_end = 0;
+		
+		for (start, _) in name.match_indices("::{{closure}}") {
+			cleaned_name.push_str(&name[prev_end..start]);
+			prev_end = start + "::{{closure}}".len();
+		}
+		
+		cleaned_name.push_str(&name[prev_end..]);
+		
+		cleaned_name
     }}
 }
--- tests/function_name.rs
+++ tests/function_name.rs
@@ -11,5 +11,10 @@ mod a_mod {
 	#[test]
 	fn function_in_mod() {
 		assert_eq!(function_name!(), "function_name::a_mod::function_in_mod");
+
+		// should remove closures
+		let _closure = || {
+			assert_eq!(function_name!(), "function_name::a_mod::function_in_mod");
+		};
 	}
 }
